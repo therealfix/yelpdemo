223654d38163111d0bdb49d7409acffebf3674ca
Kevin Peng <therealfix@yahoo.ca>
2015-09-06 12:29:35 -0400

    Add reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150906021342) do
+ActiveRecord::Schema.define(version: 20150906161158) do
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20150906021342) do
     t.datetime "updated_at"
     t.string   "phone"
     t.string   "image"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
