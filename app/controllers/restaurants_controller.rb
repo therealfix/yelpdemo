ad90ab647f49a299b7b2131bf2be67207a92efed
Kevin Peng <therealfix@yahoo.ca>
2015-09-07 15:58:50 -0400

    display reviews on restaurant page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,6 +10,8 @@ class RestaurantsController < ApplicationController
   # GET /restaurants/1
   # GET /restaurants/1.json
   def show
+    #This defines the @reviews variable to be all reviews where the Review belongs to the current restaurant.
+    @reviews = Review.where(restaurant_id: @restaurant.id)
   end
 
   # GET /restaurants/new
